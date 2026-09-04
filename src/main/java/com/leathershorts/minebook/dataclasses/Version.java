c508f7a7d0d5688062539718e8b641168354c351
LeaTHeRSHoRTs <noahth931@gmail.com>
2026-09-04 13:09:47 +1200

    Update Version.java
    Added version getters to Version.java

--- src/main/java/com/leathershorts/minebook/dataclasses/Version.java
+++ src/main/java/com/leathershorts/minebook/dataclasses/Version.java
@@ -5,18 +5,29 @@ public record Version(
     int minor,
     int patch
 ) {
-    @Override
-    public int major() {
-        return major;
+    public Version(int major, int minor) {
+        this(major, minor, 0);
     }
 
-    @Override
-    public int patch() {
-        return patch;
+    private String getVersion(InternalVersion type) {
+        return switch (type) {
+            case MAJOR -> "%d".formatted(this.major);
+            case MINOR -> "%d.%d".formatted(this.major, this.minor);
+            case PATCH -> "%d.%d.%d".formatted(this.major, this.minor, this.patch);
+        };
     }
 
-    @Override
-    public int minor() {
-        return minor;
+    public String getVersion() {
+        return this.patch == 0 ? this.getVersion(InternalVersion.MINOR) : this.getVersion(InternalVersion.PATCH);
+    }
+
+    public String getFullVersion() {
+        return this.getVersion(InternalVersion.PATCH);
+    }
+
+    private enum InternalVersion {
+        MAJOR,
+        MINOR,
+        PATCH
     }
 }
